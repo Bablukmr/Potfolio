c955a59f8bac136e024c78a415fe23e6fccc0e02
BABLU KUMAR <bablukmr800@gmail.com>
2023-06-19 11:28:05 +0530

    fixed @media

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
     <div className="container header__container">
      <h5>Hello I'm</h5>
      <h1>Bablu Kumar</h1>
-     <h5 className='text-light'>FrontEnd Developer</h5>
+     <h5 className='text-light'>Front-End Developer</h5>
      <CTA/>
      <HeaderSocial/>
      <div className="me">
